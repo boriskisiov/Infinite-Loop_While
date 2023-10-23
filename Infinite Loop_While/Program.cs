33ddefd127e324c751ee02778dda5065480b73fe
boriskisiov <boriskisiov@gmail.com>
2023-10-23 16:22:36 +0300

    Add project files.

---
+++ Infinite Loop_While/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Infinite_Loop_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            while (comand != "End") {
+             Console.WriteLine("Executing: " + comand);
+                comand = Console.ReadLine();
+            }
+        }
+    }
+}
